4e7c1cbf270870600d83a44042c1a893414f2da5
Keyapatel3012 <Keyurip42@gmail.com>
2023-07-26 15:10:00 -0400

    Update poke_image_viewer.py

--- poke_image_viewer.py
+++ poke_image_viewer.py
@@ -21,18 +21,26 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 images_dir = os.path.join(script_dir, 'images')
 
 # TODO: Create the images directory if it does not exist
-if not os.path.isdir(images_dir)
+if not os.path.isdir(images_dir):
     os.makedirs(images_dir)
 
 # Create the main window
 root = Tk()
 root.title("Awesome App")
 root.geometry('600x600')
+root.minsize(500,600)
+root.columnconfigure(0,weight=1)
+root.rowconfigure(0,weight=1)
+
 
 # TODO: Set the icon
+app_id = 'COMP593.PokeImageViewer'
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(app_id)
+root.iconbitmap(os.path.join(script_dir, 'poke_ball.ico'))
+
 
 # TODO: Create frames
 
 # TODO: Populate frames with widgets and define event handler functions
 
-root.mainloop()
+root.mainloop() 
