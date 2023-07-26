d5c08435ed542ef7a40db4a170571494d0fc0ab1
Keyapatel3012 <Keyurip42@gmail.com>
2023-07-26 14:54:44 -0400

    Update poke_image_viewer.py

--- poke_image_viewer.py
+++ poke_image_viewer.py
@@ -8,17 +8,26 @@ Usage:
 """
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
+import ctypes
 import os
+import poke_api
+import image_lib
+import inspect
 
 # Get the script and images directory
+script_name = inspect.getframeinfo(inspect.currentframe()).filename
 script_dir = os.path.dirname(os.path.abspath(__file__))
 images_dir = os.path.join(script_dir, 'images')
 
 # TODO: Create the images directory if it does not exist
+if not os.path.isdir(images_dir)
+    os.makedirs(images_dir)
 
 # Create the main window
 root = Tk()
-root.title("Pokemon Viewer")
+root.title("Awesome App")
+root.geometry('600x600')
 
 # TODO: Set the icon
 
