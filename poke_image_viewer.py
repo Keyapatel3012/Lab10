8e3033951ff1d333a8000c73d530626ae9641008
Keyapatel3012 <Keyurip42@gmail.com>
2023-08-11 23:59:13 -0400

    Update poke_image_viewer.py

--- poke_image_viewer.py
+++ poke_image_viewer.py
@@ -1,102 +1,82 @@
-"""
-Description:
-  Graphical user interface that displays the official artwork for a
-  user-specified Pokemon, which can be set as the desktop background image.
-
-Usage:
-  python poke_image_viewer.py
-"""
+import requests
 from tkinter import *
 from tkinter import ttk
-from tkinter import messagebox
-import ctypes
-import os
-import poke_api
-import image_lib
-import inspect
+from tkinter import filedialog
+from PIL import Image, ImageTk 
+from poke_api import get_pokemon_info
 
-# Get the script and images directory
-script_name = inspect.getframeinfo(inspect.currentframe()).filename
-script_dir = os.path.dirname(os.path.abspath(__file__))
-images_dir = os.path.join(script_dir, 'images')
 
-# TODO: Create the images directory if it does not exist
-if not os.path.isdir(images_dir):
-    os.makedirs(images_dir)
+
+# Function to set the desktop background
+def set_desktop_background(image_path):
+    try:
+        import ctypes
+        ctypes.windll.user32.SystemParametersInfoW(20, 0, image_path, 3)
+        print("Desktop background set successfully.")
+    except Exception as e:
+        print("Error setting desktop background:", str(e))
+
+# Function to handle button click event
+def set_desktop_image():
+    selected_pokemon = combo_var.get()
+    if selected_pokemon:
+        poke_info = get_pokemon_info(selected_pokemon)
+        if poke_info:
+            image_url = poke_info['sprites']['other']['official-artwork']['front_default']
+            response = requests.get(image_url)
+            if response.status_code == requests.codes.ok:
+                image_data = response.content
+                image_path = selected_pokemon + ".png"
+                with open(image_path, 'wb') as image_file:
+                    image_file.write(image_data)
+                set_desktop_background(image_path)
+                print("Image saved and set as desktop background.")
 
 # Create the main window
 root = Tk()
-root.title("Awesome App")
-root.geometry('600x600')
-root.minsize(500,600)
-root.columnconfigure(0,weight=1)
-root.rowconfigure(0,weight=1)
+root.title("Pokemon Image Viewer")
+root.resizable(False, False)
 
-#set the icon displayed on the upper-right corner of the window and Windows taskbar
-# The first lines of code is needed for the icon to appear in the Windows taskbar,
+# Create the frames
+frame_input = ttk.Frame(root)
+frame_input.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 
+frame_image = ttk.LabelFrame(root, text="Image")
+frame_image.grid(row=1, column=0, padx=10, pady=10)
 
+# Populate the user input frame with widgets
+label_name = ttk.Label(frame_input, text="Select a Pokemon:")
+label_name.grid(row=0, column=0, padx=5, pady=5)
 
-# TODO: Set the icon
-app_id = 'COMP593.PokeImageViewer'
-ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(app_id)
-root.iconbitmap(os.path.join(script_dir, 'poke_ball.ico'))
+# Create a Combobox to select Pokemon names
+pokemon_list = ["Pikachu", "Charmander", "Squirtle"]  # You can add more Pokemon names
+combo_var = StringVar()
+combo_box = ttk.Combobox(frame_input, textvariable=combo_var, values=pokemon_list)
+combo_box.grid(row=0, column=1, padx=5, pady=5)
 
+btn_set_image = ttk.Button(frame_input, text="Set as Desktop Image", command=set_desktop_image)
+btn_set_image.grid(row=0, column=2, padx=5, pady=5)
 
-# TODO: Create frames
-frm = ttk.Frame(root)
-frm.columnconfigure(0,weight=1)
-frm.rowconfigure(0,weight=1)
-frm.grid(sticky=NSEW)
+# Define a callback function for the Combobox selection event
+def combo_selected(event):
+    selected_pokemon = combo_var.get()
+    if selected_pokemon:
+        poke_info = get_pokemon_info(selected_pokemon)
+        if poke_info:
+            image_url = poke_info['sprites']['other']['official-artwork']['front_default']
+            response = requests.get(image_url)
+            if response.status_code == requests.codes.ok:
+                image_data = response.content
+                image = PhotoImage(data=image_data)
+                image_label.config(image=image)
+                image_label.image = image  # Keep a reference to prevent garbage collection
+                btn_set_image['state'] = 'normal'  # Enable the button
 
-#create the image viewer widget 
-image_path = os.path.join(script_dir, 'poke_ball.png')
-#method 1 - Using classes from tkinter:
-photo = PhotoImage(file=image_path)
-#
-#
-lbl_image = ttk.Label_(frm, image=photo)
-lbl_image.grid(row=0, column=0, padx=10, pady=10)
+# Create an image label in the image frame
+image_label = ttk.Label(frame_image)
+image_label.grid(row=0, column=0, padx=10, pady=10)
 
-#create the button to set deskptop bg
-def handle_set_desktop():
-    """
-    Event handler called when user clicks the "Set as Deskptop Image" button.
-    Sets the deskptop bg image to the current Pokemon display image
+# Bind the Combobox selection event to the callback function
+combo_box.bind("<<ComboboxSelected>>", combo_selected)
 
-    """
-    image_lib.set_deskptop_backgrounf_image(image_path)
-
-btn_set_desktop = ttk.Button(frm, text='Set as Desktop Image', command=handle_set_desktop)
-btn_set_desktop.state('Disabled')
-btn_set_desktop.grid(row=1,column=1,padx=0,pady=(10,20))
-
-#create list of pull down pokemon names
-pokemon_list = poke_api.get_pokemon_names()
-pokemon_list.sort()
-cbox_poke_sel = ttk.Combobox(frm, value=pokemon_list, state ='readonly')
-cbox_poke_sel.set("Select a pokemon") 
-cbox_poke_sel.grid(row=1, column=0, padx=0, pady=10)
-
-def handle_poke_sel(event):
-    global image_path
-
-    current_sel = cbox_poke_sel.get()
-    image_path = poke_api.download_pokemon_artwork(current_sel, images_dir)
-    if image_path:
-         lbl_image['text'] = None
-         photo['file'] = image_path
-        #
-        #
-        #
-        #
-        #
-        #
-    else
-        
-    
-# TODO: Populate frames with widgets and define event handler functions
-
-
-cbox_poke_sel.bind('<<ComboboxSelected>>', handle_poke_sel)
-root.mainloop() 
+root.mainloop()
